c076d4739ec18a2cbcb51e4109b2f914a1476859
santiago-semb <sansembbb@gmail.com>
2023-05-24 13:42:25 -0300

    frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { CertificadosComponent } from './certificados/certificados.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component:InicioComponent},
+  {path:'estudios', component:EstudiosComponent},
+  {path:'certificados', component:CertificadosComponent},
+  {path:'experiencia', component:ExperienciaComponent},
+  {path:'sobre-mi', component:SobreMiComponent},
+  {path:'proyectos', component:ProyectosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { CertificadosComponent } from './certificados/certificados.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { SubNavComponent } from './sub-nav/sub-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ProyectosComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    SobreMiComponent,
+    CertificadosComponent,
+    HeaderComponent,
+    NavComponent,
+    SubNavComponent,
+    FooterComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
